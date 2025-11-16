65c276eea9b87630cd1c6d88e2b2f78db5fa9b21
galih <galihadityam1@gmail.com>
2025-11-17 02:02:28 +0700

    resolve deployment issue

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/elly-test-1',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
